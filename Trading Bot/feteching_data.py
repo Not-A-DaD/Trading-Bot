94746b6904b7dd500b2babb372d0936f5e599f8d
Not-A-DaD <bhushanritik4@gmail.com>
2024-12-06 17:13:46 +0530

    feteching_data.py

--- Trading Bot/feteching_data.py
+++ Trading Bot/feteching_data.py
@@ -2,31 +2,34 @@ import ccxt
 import pandas as pd 
 from pyti.simple_moving_average import simple_moving_average
 import numpy
-import time 
+import time
+import logging
+
+logging.basicConfig(filename="Trading Bot",filemode="w")
+
 pd.set_option("display.max_rows",1000) # if u want to see the hole data frame u can use it 
+symbol = "ETH/USD"
 
 exchange = ccxt.binance()
 def fetching_ohlcv_data ():
     try:
-        symbol = "BTC/USDT"
         ohlcv = exchange.fetch_ohlcv(symbol,timeframe="15m",limit=500)
+        logging.info("Feteched Data sussecfully")
 
         close_values = [ohlcv[i][4] for i in range(len(ohlcv))]
-        ohlcv_df = pd.DataFrame(close_values)
-
-        # print(ohlcv_df)
         return close_values 
     except:
-        # Exception as 
-        print("error fetching the data")
+        logging.error("Error fetching the data")
+        print("error")
 def sma (data):
     try:
         shoeter_sma = simple_moving_average(data,50)
         longer_sma = simple_moving_average (data,200)
-    except:
-        print("error getting the SMA")
+        logging.info("SMA featched")
+    except Exception as e:
+        logging.critical(f"cant get SMA {e}")
+        return None
     
-
     sma_value = pd.DataFrame({
         "Close_Value" : data ,
         "shoeter_sma" : shoeter_sma,
@@ -35,20 +38,22 @@ def sma (data):
     sma_value.to_string(index=False)
 
 
-    sma_value["Signal"] = numpy.where(sma_value["shoeter_sma"] > sma_value["longer_sma"],"buy","sell") 
-    buy_locater = sma_value["Signal"].tail(1)
-    price_locater = sma_value["Close_Value"].tail(1)
-    print(price_locater)
-    print(f" buy_signal : {str(buy_locater)}{sma_value.to_string(index=False)}")
+    sma_value["Signal"] = numpy.where(sma_value["shoeter_sma"] > sma_value["longer_sma"],"✅","❌") 
+
+    buy_locater = sma_value["Signal"].tail(1).to_string(index=False)
+    price_locater = sma_value["Close_Value"].tail(1).to_string(index=False)
+
+
+    print(f"Price: {price_locater}  buy_signal : {str(buy_locater)}")
 
 
 
 def main():
-    data = fetching_ohlcv_data()
     while True:
-        time.sleep(10)
+        data = fetching_ohlcv_data()
+        time.sleep(10) # for testing it is 10 but try to do 15 mins accoding to your time frame  
+        # set it to 900 
         sma(data)
 
 main()
-
 
